20e6b1f109880bda4b9db6a764849ea2b35809fa
Zulfikaryehya <zyehya007@gmail.com>
2025-08-08 02:42:26 +0300

    preparation for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,22 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./Middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve(); // Get the current directory name
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-); // Enable CORS for the frontend
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  ); // Enable CORS for the frontend
+}
+
 app.use(express.json()); // Middleware to parse JSON request bodies
 //custom middleware
 app.use(rateLimiter); // Apply rate limiting middleware
@@ -28,6 +32,13 @@ app.use(rateLimiter); // Apply rate limiting middleware
 
 app.use("/api/notes", notesRouter); // will  be like a prefix for all routes in notesRouter
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // Serve static files from the frontend build directory
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html")); // Serve the index.html file for all other routes
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server is running on port ", PORT);
